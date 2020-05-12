360025f468312dc8f691a9d21a9eecea4f45e815
Carriedied <52948394+Carriedied@users.noreply.github.com>
2020-05-12 11:58:53 +0700

    Add files via upload

---
+++ Viktorina/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Viktorina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(460, 20);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(460, 40);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 168);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Visible = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(6, 127);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(14, 13);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(6, 35);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(14, 13);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(6, 81);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(14, 13);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(460, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 295);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Viktorina/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Viktorina
+{
+    public partial class Form1 : Form
+    {
+        StreamReader sr = new StreamReader(@"test.txt", System.Text.Encoding.Default);
+        int i = 0;
+    public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Начать")
+            {
+                i++;
+                button1.Text = "Далее";
+                label1.Visible = true;
+                groupBox1.Visible = true;
+                label1.Text = sr.ReadLine();
+                radioButton1.Text = sr.ReadLine();
+                radioButton2.Text = sr.ReadLine();
+                radioButton3.Text = sr.ReadLine();
+            }
+            else 
+            {
+                if (button1.Text == "Далее")
+                {
+                    if ((sr.ReadLine()) != null)
+                    {  
+                        label1.Text = sr.ReadLine();
+                        radioButton1.Text = sr.ReadLine();
+                        radioButton2.Text = sr.ReadLine();
+                        radioButton3.Text = sr.ReadLine();
+                        if (i == 1)
+                        {
+                            if (radioButton1.Checked)
+                            {
+                                progressBar1.Value += 0;
+                            }
+                            if (radioButton2.Checked)
+                            {
+                                progressBar1.Value += 25;
+                            }
+                            if (radioButton3.Checked)
+                            {
+                                progressBar1.Value += 0;
+                            }
+                        }
+                        if (i == 2)
+                        {
+                            if (radioButton1.Checked)
+                            {
+                                progressBar1.Value += 0;
+                            }
+                            if (radioButton2.Checked)
+                            {
+                                progressBar1.Value += 0;
+                            }
+                            if (radioButton3.Checked)
+                            {
+                                progressBar1.Value += 25;
+                            }
+                        }
+                        if (i == 3)
+                        {
+                            if (radioButton1.Checked)
+                            {
+                                progressBar1.Value += 0;
+                            }
+                            if (radioButton2.Checked)
+                            {
+                                progressBar1.Value += 25;
+                            }
+                            if (radioButton3.Checked)
+                            {
+                                progressBar1.Value += 0;
+                            }
+                        }
+                        i++;
+                    }
+                    else
+                    {
+                        if (i == 4)
+                        {
+                            if (radioButton1.Checked)
+                            {
+                                progressBar1.Value += 25;
+                            }
+                            if (radioButton2.Checked)
+                            {
+                                progressBar1.Value += 0;
+                            }
+                            if (radioButton3.Checked)
+                            {
+                                progressBar1.Value += 0;
+                            }
+                        }
+                        MessageBox.Show("Вы прошли тест на " + progressBar1.Value + "%");
+                        this.Close();
+                    }
+                }
+            }
+        }
+    }
+}
